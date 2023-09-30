6606e41754a712d10b05556715ebdcb8cf00bc13
Swarnim B <swarnim14.9@hotmail.com>
2023-09-30 22:44:08 +0530

    wrapped func
    wrapped tail recursive function in another function to abstract away the extra int that needs to be oassed since its initial value is always 0

--- src/04-list-len.ml
+++ src/04-list-len.ml
@@ -4,13 +4,15 @@ let rec list_len (xs: 'a list): int =
   | x :: rest -> 1 + list_len rest
 
 (* Tail Recursive Solution *)
-let rec tail_list_len (k: int) (xs: 'a list): int =
-  match xs with
-  | [] -> k
-  | _ :: rest -> tail_list_len (k + 1) rest
+let tail_list_len (xs: 'a list): int =
+  let rec tail_list_len' (k: int) (xs: 'a list): int =
+    match xs with
+    | [] -> k
+    | _ :: rest -> tail_list_len' (k + 1) rest
+  in tail_list_len' 0 xs
 
 let my_list = [1; 2; 3; 4; 5; 6]
 
 let () =
-  match tail_list_len 0 my_list with
+  match tail_list_len my_list with
   | x -> Printf.printf "The length of the list is %d\n" x
