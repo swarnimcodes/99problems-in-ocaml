655d60797e502825c41070d2853544d480e3261a
Swarnim B <swarnim14.9@hotmail.com>
2023-09-30 22:40:07 +0530

    added tail recursive solution

--- src/04-list-len.ml
+++ src/04-list-len.ml
@@ -3,8 +3,14 @@ let rec list_len (xs: 'a list): int =
   | [] -> 0
   | x :: rest -> 1 + list_len rest
 
+(* Tail Recursive Solution *)
+let rec tail_list_len (k: int) (xs: 'a list): int =
+  match xs with
+  | [] -> k
+  | _ :: rest -> tail_list_len (k + 1) rest
+
 let my_list = [1; 2; 3; 4; 5; 6]
 
 let () =
-  match list_len my_list with
+  match tail_list_len 0 my_list with
   | x -> Printf.printf "The length of the list is %d\n" x
